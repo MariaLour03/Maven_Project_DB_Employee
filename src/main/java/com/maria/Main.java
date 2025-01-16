6428212a5fceb425163222fbfd4f30ccfb231f89
MariaLour03 <133254367+MariaLour03@users.noreply.github.com>
2025-01-15 23:16:22 -0600

    Update Main.java

--- src/main/java/com/maria/Main.java
+++ src/main/java/com/maria/Main.java
@@ -8,11 +8,11 @@ public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         EmployeeService employeeService = new EmployeeService();
-        // employeeService.getAllEmployees();
-        // employeeService.getEmployeeById();
-        // employeeService.getEmployeeByIdAndName();
-        // employeeService.InsertEmployee();
+        employeeService.getAllEmployees();
+        employeeService.getEmployeeById();
+        employeeService.getEmployeeByIdAndName();
+        employeeService.InsertEmployee();
         employeeService.updateEmployee();
-        //employeeService.useSQLQueries();
+        employeeService.useSQLQueries();
     }
 }
